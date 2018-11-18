d9d2d5d9fc16be795a979ad33338663aff9a359d
Pradeep <k.pradeep12@outlook.com>
2018-11-17 23:59:36 -0500

    config change

--- movie-service/src/main/kotlin/com/example/movieservice/clients/ReviewsFeignClient.kt
+++ movie-service/src/main/kotlin/com/example/movieservice/clients/ReviewsFeignClient.kt
@@ -3,10 +3,11 @@ package com.example.movieservice.clients
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
-@FeignClient("reviews-service")
+@FeignClient(value = "reviews-service", url = "http://localhost:8761")
 interface ReviewsFeignClient{
 
-    @RequestMapping("/reviews/{movieId}")
+    @RequestMapping(method = [RequestMethod.GET], value = ["/reviews/{movieId}"])
     fun getReviews(@PathVariable("movieId") movieId:String) : Array<String>
 }
